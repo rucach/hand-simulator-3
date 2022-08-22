6e7cc9956f91884c35f8cddeb9e026fadcf7eca1
Rucach <98107806+rucach@users.noreply.github.com>
2022-08-22 19:00:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,28 +5,45 @@ Webcam.set({
     png_quality: 90
 });
 
-camera = document.getElementById("result");
+camera = document.getElementById("camera");
 
-webcam.attach('#camera');
-
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uSIBSSHlV/model.json', modelLoaded);
-
-console.log('ml5 version: ', ml5.version);
-
-function modelLoaded(){
-    console.log("model loaded");
-}
+Webcam.attach('#camera');
 
 function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
-    });
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+});
+}
+console.log('ml5 version. ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0A9kEYdem/model.json', modelLoaded);
+
+function modelLoaded(){
+console.log("model loaded!");
 }
 
 function speak(){
-    var synth = window.speechSynthesis;
-    speak_data1 = "first prediction is " + prediction_1;
-    speak_data2 = "second prediction is " + prediction_2;
-    var utter_this = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
-    synth.speak(utter_this);
+var synth = window.speechSynthesis;
+speak_data1 = "the first prediction is " + prediction_1;
+speak_data2 = "the second prediction is " + prediction_2;
+var utter_this = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
+synth.speak(utter_this);
+}
+
+function check(){
+img = document.getElementById("captured_image");
+classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if(error){
+    console.error(error);
+}else{
+    console.log(results);
+    document.getElementById("result_imotion_name").innerHTML = results[0].label;
+    document.getElementById("result_imotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+}
 }
